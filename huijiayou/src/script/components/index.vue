f19b90e1c83a6a9d95790c7d2393f6d96658c44b
houwh <315176653@qq.com>
2017-05-04 21:35:09 +0800

    曹居伟新

---
+++ huijiayou/src/script/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="m-index">
+		<header>
+			awdawdwad
+		</header>
+		<section>
+			
+		</section>
+		<nav>
+			
+		</nav>
+	</div>
+</template>
+
+<style lang="scss">
+$background : blue;
+	.m-index{
+		height: 100%;
+		width: 100%;
+		background: $background;
+	}
+</style>
