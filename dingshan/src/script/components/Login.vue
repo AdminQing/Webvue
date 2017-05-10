76d62ba515a372ce3250ae8389159bcbc007a86a
liucheng <18451790560@163.com>
2017-05-10 19:59:57 +0800

    '购物车初步'

---
+++ dingshan/src/script/components/Login.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+
+<div class="login-main">
+  <div class="yo-header">
+    <h2 class="title">登录</h2>
+    <span @click="back" class="regret yo-ico"></span>
+  </div>
+	<div class="login-logo">
+
+	</div>
+	<form class="login-form" method="post">
+			<input type="text" name="username" placeholder="请输入邮箱/用户名/手机" class="username" required>
+			<input type="password" name="pwd" placeholder="请输入密码" class="pwd" required>
+
+      <input type="checkbox" name="" class="check">
+			<span class="rem">记住密码</span>
+			<span class="for">忘记密码</span>
+
+			<input type="submit" name="" value="登录" class="login1" >
+			<input type="button" name="" value="微信登录" class="login2" >
+      <router-link  class="tore" to="/register" >
+        <span>免费注册</span>
+      </router-link>
+	</form>
+</div>
+
+</template>
+
+<script>
+export default {
+  methods: {
+    back: function () {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style l
--- dingshan/src/script/components/Shop.vue
+++ dingshan/src/script/components/Shop.vue
@@ -1,5 +1,48 @@
 <template>
-	<div class="shop">
-		购物车
+	<div class="shop-main">
+		<div class="shop-none">
+			<p>购物车空空如也，快去购物吧</p>
+			<input type="button" name="" value="去购物">
+		</div>
+		<div class="line_01">
+			<div class="line-left">
+			</div>
+			看了又看
+			<div class="line-right">
+			</div>
+		</div>
+		<div class="shop-more">
+			<ul >
+				<li v-for='(item,index) in dataSource' :key="index">
+					<img :src="`http://d.zwzpy.com/upload/${item.gos_thumbnail}`" alt="">
+					<p class="shop-name">{{item.gos_name}}</p>
+					<p class="shop-price">￥{{item.gos_price}}</p>
+					<input type="button" name="" value="立即购买">
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
+<script>
+import Vue from 'vue'
+import utilAxios from '../utils/axios'
+export default {
+	data(){
+		return {
+			dataSource:[]
+		}
+	},
+	mounted:function(){
+		let that=this;
+		utilAxios.get({
+        url: '/m/index.php?ac=cart_ajaxlists',
+        method: 'get',
+        callback: function (res) {
+					console.log(res)
+          that.dataSource=that.dataSource.concat(res.data.goodlist);
+
+        }
+      })
+	}
+}
+</script>
